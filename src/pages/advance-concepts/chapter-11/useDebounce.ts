c85dc18a3cddae51d0b0ec052572de358548839c
Sam Tey <samazingg96@gmail.com>
2023-10-12 21:49:47 +0800

    update: add comments

--- src/pages/advance-concepts/chapter-11/useDebounce.ts
+++ src/pages/advance-concepts/chapter-11/useDebounce.ts
@@ -6,15 +6,23 @@ const useDebounce = (callBack: () => void) => {
     const ref = useRef<any>()
 
     useEffect(() => {
+        //update ref.current with callBack
+        //ref.current here will access to the latest callBack
         ref.current = callBack
     }, [callBack])
 
+    /**
+     * Create it only once - on mount
+     */
     const debouncedCallback = useMemo(() => {
+        // func will create only once - on mount
         const func = () => {
+            //ref is mutable, it access to the latest callBack
             ref.current?.()
         }
 
         return debounce(func, 1000)
+
     }, [])
 
     return debouncedCallback
